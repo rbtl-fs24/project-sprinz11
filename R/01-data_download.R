35af44b98c27814654d45b4f4249bee3ecbd3d3e
sprinz11 <sprinz@student.ethz.ch>
2024-04-09 17:57:11 +0000

    Setup for capstone project

---
+++ R/01-data_download.R
@@ -0,0 +1,5 @@
+# You will use the data_download.R file to download your data from the Google
+# Sheet that you have established for your capstone project. Use the googlesheets4
+# package to access the data. Do not clean the data, but save it in the data/raw
+# folder using the write_csv() function. Give the the a meaningful name that
+# describes your project.
---
+++ R/02-data_cleaning.R
@@ -0,0 +1,6 @@
+# You will use the data_cleaning.R file to process your data into a state that
+# is ready for analysis. Do not access the data from Google Drive, but rather
+# use the read_csv() function to access your data from the data/raw folder. Save
+# the processed, analysis-ready data in the data/processed folder using the
+# write_csv() function. Again, give the file a meaningful name that describes
+# your project.
